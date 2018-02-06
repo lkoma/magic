877da9a0856f28599105f2eb45df3b2a2dcf7ee2
lkoma <oceanaly@163.com>
2018-02-06 17:42:39 +0800

    add signature

--- src/homepage/router/index.js
+++ src/homepage/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Homepage from 'homepage/views/index';
+import Signature from 'homepage/views/signature';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'Homepage',
             component: Homepage
+        },
+        {
+            path: '/signature',
+            name: 'Signature',
+            component: Signature
         }
     ]
 });
---
+++ src/homepage/views/signature.vue
@@ -0,0 +1,59 @@
+<template>
+   <div>
+       <mu-appbar title="签名"></mu-appbar>
+        <mu-raised-button label="点我签名" primary @click="popup"/>
+        <div>
+            <img :src="image" alt="" class="img">
+        </div>
+        <mu-popup position="top" :overlay="true" popupClass="demo-popup-top" :open="show" @close="close">
+             <mu-content-block>
+                <vueSignature ref="signature" :sigOption="option" :w="'400px'" :h="'400px'"></vueSignature>
+                <vueSignature ref="signature1" :sigOption="option"></vueSignature>
+                <mu-raised-button label="保存" primary @click="save"/>
+                <mu-raised-button label="清除" primary @click="clear"/>
+            </mu-content-block>
+        </mu-popup>
+   </div>
+</template>
+
+<script>
+import vueSignature from 'vue-signature';
+import Vue from 'vue';
+
+Vue.use(vueSignature);
+
+export default {
+    data() {
+        return {
+            show: false,
+            option: {
+                penColor: 'rgb(0, 0, 0)'
+            },
+            image: ''
+        };
+    },
+    created() {
+    },
+    methods: {
+        popup() {
+            this.show = true;
+        },
+        close() {
+            this.show = false;
+        },
+        save() {
+            const png = this.$refs.signature.save();
+            console.log(png);
+            this.image = png;
+            this.show = false;
+        },
+        clear() {
+            this.$refs.signature.clear();
+        }
+    }
+};
+</script>
+<style lang="stylus" scoped>
+.img
+    size 150px 150px
+</style>
